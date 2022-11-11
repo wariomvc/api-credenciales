f3bcaad4704e8d05c0c294e6cf5f61dd80b4c648
Mario Valenzuela (wariomvc) <wario.mvc@gmai.com>
2022-11-11 12:16:38 -0600

    Cambio en la estrucutra del Server de BD

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express from 'express';
-const { dbConnection } = require('./utils/database/config')
+import db from './utils/database/config'
 
 const app = express()
 app.use(express.json())
 
 app.use('/api/clientes', require('./routers/clientes.routers'))
-app.get('/', (_, res) => {
-  console.log('holka')
-})
+
+const dbConnection = async () => {
+  try {
+    await db.authenticate()
+    console.log('Conexion establecida con la BD')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 dbConnection()
 app.listen(3000, () => {
   console.log('escuchando')
