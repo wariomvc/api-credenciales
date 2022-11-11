6f468510aa352db429bd9d28e2599435ef515512
Mario Valenzuela (wariomvc) <wario.mvc@gmai.com>
2022-11-11 12:16:08 -0600

    Ajuste en la Conexion de la BD

--- src/utils/database/config.ts
+++ src/utils/database/config.ts
@@ -1,18 +1,10 @@
 import { Sequelize } from 'sequelize-typescript'
 
-const sequelize = new Sequelize({
+const db = new Sequelize({
   host: 'localhost',
   dialect: 'mysql',
   username: 'root',
   password: 'zelda128',
 })
-const dbConnection = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Conexion establecida con la BD')
-  } catch (error) {
-    console.error(error)
-  }
-}
 
-module.exports = { dbConnection }
+export default db
