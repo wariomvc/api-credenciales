200c8273546ce7201ed35ae86af1ee3eda2202db
Mario Valenzuela (wariomvc) <wario.mvc@gmai.com>
2022-11-12 22:45:51 -0600

    Reestructurado a OOP la configuracion de la BD

--- src/utils/database/config.ts
+++ src/utils/database/config.ts
@@ -1,10 +1,23 @@
 import { Sequelize } from 'sequelize-typescript';
 
-const db = new Sequelize({
-  host: 'localhost',
-  dialect: 'mysql',
-  username: 'root',
-  password: 'zelda128',
-});
+class Database {
+  public db: Sequelize;
+  constructor() {
+    this.db = new Sequelize({
+      host: 'localhost',
+      dialect: 'mysql',
+      username: 'root',
+      password: 'zelda128',
+    });
+  }
+  async connect() {
+    try {
+      await this.db.authenticate();
+      console.log('Conexion establecida con la BD');
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
 
-export default db;
+export default Database;
