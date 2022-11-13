ddecfb214f615594bef33e41a36d7194b7529030
Mario Valenzuela (wariomvc) <wario.mvc@gmai.com>
2022-11-12 22:46:06 -0600

    Creado modelo Cliente en Sequelize

---
+++ src/models/Cliente.ts
@@ -0,0 +1,21 @@
+import { DataTypes } from 'sequelize';
+
+import db from '../utils/database/connection';
+
+const Cliente = db.define('Cliente', {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  nombre: { type: DataTypes.STRING, allowNull: false },
+  apellido: { type: DataTypes.STRING, allowNull: false },
+  nacimiento: { type: DataTypes.STRING, allowNull: false },
+  tutor: { type: DataTypes.STRING },
+  escuela: { type: DataTypes.STRING, allowNull: false },
+  telefono: { type: DataTypes.STRING },
+  nivel: { type: DataTypes.ENUM('Kinder', 'Primaria', 'Secundaria') },
+  colonia: { type: DataTypes.STRING },
+  foto: { type: DataTypes.STRING },
+  codigo: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false },
+  loocutor: { type: DataTypes.ENUM('Andres Oviedo', 'Deliz Guerrero', 'Jas Herrera') },
+});
+
+export default Cliente;
