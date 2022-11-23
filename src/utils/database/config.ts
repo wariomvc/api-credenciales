d06ece00d7493a15315942491ba9edae4996d567
Mario Valenzuela (wariomvc) <wario.mvc@gmai.com>
2022-11-23 08:18:29 -0600

    Config DAtabse

--- src/utils/database/config.ts
+++ src/utils/database/config.ts
@@ -3,17 +3,26 @@ import Usuario from '../../models/Usuario';
 import Cliente from '../../models/Cliente';
 
 class Database {
-  /* public static db: Sequelize = new Sequelize({
-    database: 'appcredenciales',
-    host: 'localhost',
+  /*   public static db: Sequelize = new Sequelize({
+    database: 'V8tUnq1239',
+    host: '212.47.237.65',
     dialect: 'mysql',
-    username: 'root',
-    password: 'zelda128',
+    username: 'V8tUnq1239',
+    password: 'CSw8tgVA2e',
   }); */
-  public static db: Sequelize = new Sequelize({
+  MYSQLUSER = 'root';
+  MYSQLPASSWORD = '48QsVnL0UHuCrXAMxnwm';
+  MYSQLHOST = 'containers-us-west-57.railway.app';
+  MYSQLPORT = '7886';
+  MYSQLDATABASE = 'railway';
+  public static db = new Sequelize(
+    `mysql://root:48QsVnL0UHuCrXAMxnwm@containers-us-west-57.railway.app:7886/railway`,
+  ); // Example for postgres
+
+  /* public static db: Sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: 'database.sqlite',
-  });
+  }); */
   constructor() {
     Database.db.modelManager.addModel(Usuario);
     Database.db.modelManager.addModel(Cliente);
--- src/utils/database/connection.ts
+++ src/utils/database/connection.ts
@@ -1,8 +1,19 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'database.sqlite',
-});
-
+/* const db = new Sequelize({
+  database: 'railway',
+  host: 'containers-us-west-57.railway.app',
+  dialect: 'mysql',
+  username: 'root',
+  password: '48QsVnL0UHuCrXAMxnwm',
+}); */
+const MYSQLUSER = 'root';
+const MYSQLPASSWORD= '48QsVnL0UHuCrXAMxnwm';
+const MYSQLHOST ='containers-us-west-57.railway.app';
+ const MYSQLPORT = '7886';
+const MYSQLDATABASE = 'railway';
+//const db = new Sequelize(`mysql://${MYSQLUSER}:${{ MYSQLPASSWORD }}@${{ MYSQLHOST }}:${{ MYSQLPORT }}/${{ MYSQLDATABASE }}`) ;// Example for postgres
+const db = new Sequelize(
+  `mysql://root:48QsVnL0UHuCrXAMxnwm@containers-us-west-57.railway.app:7886/railway`,
+);
 export default db;
