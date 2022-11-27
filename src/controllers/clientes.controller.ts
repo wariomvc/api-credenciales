988ec9f586ca2ff4160d08d0a994c5b450a57d2a
Mario Valenzuela (wariomvc) <wario.mvc@gmai.com>
2022-11-26 19:21:02 -0600

    .

--- src/controllers/clientes.controller.ts
+++ src/controllers/clientes.controller.ts
@@ -472,7 +472,7 @@ const convertPDftoPNG = async (pdfFilePath: string, filename: string) => {
   const pngPages: PngPageOutput[] = await pdfToPng(
     pdfFilePath, // The function accepts PDF file path or a Buffer
     {
-      disableFontFace: true, // When `false`, fonts will be rendered using a built-in font renderer that constructs the glyphs with primitive path commands. Default value is true.
+      disableFontFace: false, // When `false`, fonts will be rendered using a built-in font renderer that constructs the glyphs with primitive path commands. Default value is true.
       useSystemFonts: true, // When `true`, fonts that aren't embedded in the PDF document will fallback to a system font. Default value is false.
       viewportScale: 1.0, // The desired scale of PNG viewport. Default value is 1.0.
       outputFolder: path.credenciales, // Folder to write output PNG files. If not specified, PNG output will be available only as a Buffer content, without saving to a file.
@@ -480,7 +480,7 @@ const convertPDftoPNG = async (pdfFilePath: string, filename: string) => {
       pdfFilePassword: '', // Password for encrypted PDF.
       pagesToProcess: [1], // Subset of pages to convert (first page = 1), other pages will be skipped if specified.
       strictPagesToProcess: false, // When `true`, will throw an error if specified page number in pagesToProcess is invalid, otherwise will skip invalid page. Default value is false.
-      verbosityLevel: 5, // Verbosity level. ERRORS: 0, WARNINGS: 1, INFOS: 5. Default value is 0.
+      verbosityLevel: 1, // Verbosity level. ERRORS: 0, WARNINGS: 1, INFOS: 5. Default value is 0.
     },
   );
   return pngPages;
