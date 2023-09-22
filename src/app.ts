b91ac41b5474be1682048443b0503737c4ea1982
Mario Valenzuela <wario.mvc@gmail.com>
2023-09-22 13:17:16 -0600

    Fix 5

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,9 @@ import dotenv from 'dotenv'; //Libreria para usar variables de entorno
 
 import Database from './utils/database/config'; // Modulo que manega la configuración de la BD
 import Server from './utils/server'; // Servidor de express
-const result = dotenv.config();
 
-if (result.error) {
-  throw result.error;
-}
+dotenv.config(); //Carga la configuraición de las variables de entorno
 
-console.log(result.parsed);
 // Genera instancia del servidor
 const database = new Database(); // Genera uns intanacia de la Basededato
 
