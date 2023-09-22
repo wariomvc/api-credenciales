aa0ea547fa292d8bf04934d1d26c52e5c979157f
Mario Valenzuela <wario.mvc@gmail.com>
2023-09-22 13:13:56 -0600

    Fix dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
-import dotenv from 'dotenv'; //Libreria para usar variables de entorno
+import 'dotenv/config'; //Libreria para usar variables de entorno
 
 import Database from './utils/database/config'; // Modulo que manega la configuración de la BD
 import Server from './utils/server'; // Servidor de express
-
-dotenv.config(); //Carga la configuraición de las variables de entorno
 
 // Genera instancia del servidor
 const database = new Database(); // Genera uns intanacia de la Basededato
--- src/utils/database/connection.ts
+++ src/utils/database/connection.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-
+import 'dotenv/config';
 
 
 console.log('***********',process.env.DATABASE);
